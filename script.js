9d19178b7e2935ee2f324e921651d7712838cdda
JTClarenwood <jacktc93@gmail.com>
2022-10-25 20:55:55 +1000

    Add haiku components

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+// Store components for haiku.
+const haikuComponents = {
+    firstFiveSyl : [
+        'Like crunchy cornflakes',
+        'The chill, worming in',
+        'I was in fire',
+        'Calm as a river',
+        'Beautiful sunrise'
+    ],
+    sevenSyl: [
+        'Gold leaves rustle underfoot',
+        'Shock, pleasure, bursting within',
+        'The room was dark and somber',
+        'Tranquility in my heart',
+        'On a warm summer morning'
+    ],
+    lastFiveSyl: [
+        'Beauty in decay',
+        'Summer tongue awakes',
+        'I sleep peacefully',
+        'Blue summer skies reign',
+        'I wait for day\'s start'
+    ]
+};
